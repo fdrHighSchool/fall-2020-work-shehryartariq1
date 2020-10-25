b0e822cb54e7dcc373ccb2bc1a82751d79e89211
shehryartariq <tm678123@gmail.com>
2020-10-25 13:21:18 -0400

    fixed indents

--- 06-math/distance.java
+++ 06-math/distance.java
@@ -1,36 +1,33 @@
 import java.util.*;
-
 class Distance {
-	
     public static void main(String[] args) {
 		
-
         cartesianDistance();
         //taxiCabDistance();
 	}
-		
+	
+
     public static void cartesianDistance(){
-		Scanner input = new Scanner(System.in);
-        double X1,X2,Y2,Y1,cartDistance;
+		
+        Scanner input = new Scanner(System.in);
+        int X1,X2,Y2,Y1;
+        double cartDistance;
 
         System.out.print("Enter the first x1 point: ");
-        X1 = input.nextDouble();
+        X1 = input.nextInt();
 
         System.out.print("Enter the first x2 point: ");
-        X2 = input.nextDouble();
+        X2 = input.nextInt();
 
         System.out.print("Enter the first y1 point: ");
-        Y1 = input.nextDouble();
+        Y1 = input.nextInt();
 
         System.out.print("Enter the first y2 point: ");
-        Y2 = input.nextDouble();
+        Y2 = input.nextInt();
 
         cartDistance = Math.sqrt(Math.pow(X2-X1,2)) + (Math.pow(Y2-Y1,2));
+        //cartDistance = Math.sqrt((X2-X1)*(X2-X1)) + ((Y2-Y1)*(Y2-Y1));
         System.out.println("The distance between 2 points: " + cartDistance);
 	}
 	
 }
-
-
-    
-
