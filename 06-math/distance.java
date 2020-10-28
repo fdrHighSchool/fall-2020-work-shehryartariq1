11218a796c11fe8778aa3f8236b18213e6050a69
shehryartariq <tm678123@gmail.com>
2020-10-28 16:58:32 -0400

    done with the distance code

--- 06-math/distance.java
+++ 06-math/distance.java
@@ -1,33 +1,63 @@
 import java.util.*;
 class Distance {
     public static void main(String[] args) {
-		
-        cartesianDistance();
-        //taxiCabDistance();
-	}
-	
+      Scanner s = new Scanner(System.in);
 
-    public static void cartesianDistance(){
-		
-        Scanner input = new Scanner(System.in);
-        int X1,X2,Y2,Y1;
-        double cartDistance;
+      System.out.print("Enter point 1: ");
+      String point1 = s.nextLine();
 
-        System.out.print("Enter the first x1 point: ");
-        X1 = input.nextInt();
+      System.out.print("Enter point 2: ");
+      String point2 = s.nextLine();
 
-        System.out.print("Enter the first x2 point: ");
-        X2 = input.nextInt();
 
-        System.out.print("Enter the first y1 point: ");
-        Y1 = input.nextInt();
+      int x1 = getX(point1);
+      int x2 = getX(point2);
+      int y1 = getY(point1);
+      int y2 = getY(point2);
 
-        System.out.print("Enter the first y2 point: ");
-        Y2 = input.nextInt();
+      cartesianDistance(x1, x2, y1, y2);
+      taxicabDistance(x1, x2, y1, y2);
+    }//end public
 
-        cartDistance = Math.sqrt(Math.pow(X2-X1,2)) + (Math.pow(Y2-Y1,2));
-        //cartDistance = Math.sqrt((X2-X1)*(X2-X1)) + ((Y2-Y1)*(Y2-Y1));
-        System.out.println("The distance between 2 points: " + cartDistance);
-	}
-	
-}
+
+    public static void cartesianDistance(int x1, int x2, int y1, int y2){
+
+      double cartDistance = Math.sqrt(Math.pow((x2-x1), 2)+ Math.pow((y2-y1), 2));
+      System.out.println("The distance between 2 points: " + cartDistance);
+
+    }//end cartesianDistance
+
+
+    public static double taxicabDistance(int x1,int x2,int y1,int y2){
+
+      double CabDistance = Math.abs(x2 - x1) + Math.abs(y2 - y1);
+      System.out.println("The distance between 2 points: " + CabDistance);
+      return CabDistance;
+
+    }//end taxicabDistance
+
+
+    public static int getX(String point){
+
+      //locate the comma
+      int comma = point.indexOf(",");
+
+    //we know the x-value in the String starts at index 1
+    //we now know where the comma is
+    //so, the x-value is from index 1 to the location of the comma
+      String x = point.substring(1, comma);
+    //System.out.println(x);
+
+      return Integer.parseInt(x);
+
+    }//end getX method
+    public static int getY(String point){
+      int ClosingBra = point.indexOf(")");
+
+      int comma = point.indexOf(",");
+
+      String y = point.substring(comma + 2, ClosingBra);
+      return Integer.parseInt(y);
+
+    }//end getY method
+}//end
